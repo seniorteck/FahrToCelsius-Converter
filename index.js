66a155a327ba0849d48cbf036e5e028776ffe69f
code_kareem <seniorteck4real@gmail.com>
2021-05-18 04:41:59 -0400

    added a max-width to the wrapper class

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const form = document.querySelector('form');
 
 // an event listener that listen for user input in the text field //
 
+const convertFahrToCelsius = (input)=>{
+    let fahrSub = input - 32;
+    let fahrMul = fahrSub * 5/9
+    return fahrMul; 
+    
+};
+
+let result = 
+convertFahrToCelsius(0);
+
+console.log(result);
+
 form.addEventListener('submit', (evt)=>{
     // stops the form from refreshing/reloading on click of submit button//
     evt.preventDefault();
     console.log('coming out strong');
-    let result = form.value;
-    console.log(result);
+    // let result = form.value;
+    // console.log(`the type input is ${result}`);
 });
