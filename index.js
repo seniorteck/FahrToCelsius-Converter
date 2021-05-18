d7deaf7fd5790eb3591f7be7b7cd6b0a2a547325
code_kareem <seniorteck4real@gmail.com>
2021-05-17 20:12:23 -0400

    modify css, removing ununsed style line, remove outline on submit-button, and added hover for submit btn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // a ref to the form tag in html
 const form = document.querySelector('form');
 
-form.addEventListener('submit', ()=>{
+// an event listener that listen for user input in the text field //
+
+form.addEventListener('submit', (evt)=>{
+    // stops the form from refreshing/reloading on click of submit button//
+    evt.preventDefault();
     console.log('coming out strong');
-})
+    let result = form.value;
+    console.log(result);
+});
